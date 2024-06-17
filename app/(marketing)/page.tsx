eb207074f5b0337b37cd391476191720102378ab
hodokim <hodo323@naver.com>
2024-06-17 22:24:58 +0900

    구조 수정

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,6 @@
-import { Footer } from "./(marketing)/_components/footer";
-import { Heading } from "./(marketing)/_components/heading";
-import { Heroes } from "./(marketing)/_components/heroes";
+import { Footer } from "./_components/footer";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
 
 const MarketingPage = () =>{
     return (

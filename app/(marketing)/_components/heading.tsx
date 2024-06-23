7b45d8f86a8061d499c8d516129be9481c885251
hodokim <hodo323@naver.com>
2024-06-23 18:25:52 +0900

    Convex, Clerk를 이용하여 가입,로그인,인증,세션 관리를 한번에!

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -2,9 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
-import { Heroes } from "./heroes";
+import {useConvexAuth} from "convex/react";
+import {Spinner} from "@/components/spinner";
+import Link from "next/link";
+import {SignInButton} from "@clerk/clerk-react";
 
 export const Heading = () => {
+    const {isAuthenticated,isLoading} = useConvexAuth();
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -16,10 +21,27 @@ export const Heading = () => {
                 Bnote와 함께하는 깔끔한 계획과 정리. <br />
                 어서 시작해보세요!
             </h3>
-            <Button>
-                Enter Bnote
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        Enter Bnote
+                    <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Bnote free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }

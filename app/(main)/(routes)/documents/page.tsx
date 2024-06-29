2f3f2698b8357b4cbba5d68cb1cde114f83b0a34
hodokim <hodo323@naver.com>
2024-06-29 23:16:05 +0900

    [24.06.29] documents 페이지 다듬기, 사이드바 다듬기

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,35 @@
+"use client"
+
+import Image from "next/image";
+import {useUser} from "@clerk/clerk-react";
+import {PlusCircle} from "lucide-react";
+
+import {Button} from "@/components/ui/button";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return (
-        <div>
-            This is a protected DocumentsPage
+        <div className={"h-full flex flex-col items-center justify-center space-y-4"}>
+            <Image src={"/empty.png"}
+                   height={"300"}
+                   width={"300"}
+                   className={"dark:hidden"}
+                   alt={"emptyPage"}
+            />
+            <Image src={"/empty-dark.png"}
+                   height={"300"}
+                   width={"300"}
+                   className={"hidden dark:block"}
+                   alt={"emptyPage"}
+            />
+            <h2 className={"text-lg font-medium"}>
+                {user?.firstName}님 환영합니다.
+            </h2>
+            <Button>
+                <PlusCircle className={"h-4 w-4 mr-2"}/>
+                새 노트 만들기
+            </Button>
         </div>
     );
 }

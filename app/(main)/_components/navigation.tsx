32194ff7390c388c9223da502c4c782be2ccaeb5
hodokim <hodo323@naver.com>
2024-07-02 21:51:15 +0900

    [24.07.01] 사이드바에 Search 와 Settings에 대한 UI 추가

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {ChevronsLeft, MenuIcon, PlusCircle} from "lucide-react";
+import {ChevronsLeft, MenuIcon, PlusCircle, Search, Settings} from "lucide-react";
 import React, {ElementRef, useEffect, useRef, useState} from "react";
 import {useMediaQuery} from "usehooks-ts";
 import {usePathname} from "next/navigation";
@@ -137,6 +137,17 @@ export const Navigation = () =>{
                 </div>
                 <div>
                     <UserItem />
+                    <Item
+                        label="Search"
+                        icon={Search}
+                        isSearch
+                        onClick={()=>{}}
+                    />
+                    <Item
+                        label="Settings"
+                        icon={Settings}
+                        onClick={()=>{}}
+                    />
                     <Item
                         onClick={handleCreate}
                         label="New page"

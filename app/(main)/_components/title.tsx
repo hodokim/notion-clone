00953cd38881aa082e5e066145356246bb5f1a6e
hodokim <hodo323@naver.com>
2024-10-30 20:05:02 +0900

    [24.10.30] 제목 수정 기능

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -4,7 +4,7 @@ import {useMutation} from "convex/react";
 
 import {Doc} from "@/convex/_generated/dataModel";
 import {api} from "@/convex/_generated/api";
-import {useState} from "react";
+import React, {useRef, useState} from "react";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 
@@ -14,18 +14,59 @@ interface TitleProps {
 export const Title = ({
     initialData
 }:TitleProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const update = useMutation(api.documents.update);
+    const [title, setTitle] = useState(initialData.title || "제목 없음");
     const [isEditing, setIsEditing] = useState(false);
+
+    const enableInput = () => {
+        setTitle(initialData.title);
+        setIsEditing(true);
+        setTimeout(()=>{
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(0, inputRef.current.value.length)
+        }, 0);
+    }
+
+    const disableInput =() => {
+        setIsEditing(false);
+    }
+
+    const onChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTitle(event.target.value);
+        update({
+            id: initialData._id,
+            title: event.target.value || "제목 없음"
+        });
+    };
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if(event.key ==='Enter'){
+            disableInput();
+        }
+    };
+
+
     return (
         <div className="flex items-center gap-x-1">
             {!!initialData.icon && <p>{initialData.icon}</p>}
             {isEditing ? (
                 <Input
+                    ref={inputRef}
+                    onClick={enableInput}
+                    onBlur={disableInput}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={title}
                     className="h-7 px-2 focus-visible:ring-transparent"
                 />
             ) : (
                 <Button
-                    onClick={() => {}}
+                    onClick={enableInput}
                     variant="ghost"
                     size="sm"
                     className="font-normal h-auto p-1"

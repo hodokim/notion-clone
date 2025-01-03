ca1d3da32ffb8ecfcfab48783e53a15349878a0f
hodokim <hodo323@naver.com>
2025-01-03 15:00:45 +0900

    [25.01.01] 페이지 로딩 스켈레톤

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -24,13 +24,13 @@ const DocumentIdPage = ({
     if(document === undefined) {
         return(
             <div>
-                {/*노트를 찾는 중입니다...*/}
                 <Cover.Skeleton />
                 <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
                     <div className="space-y-4 pl-8 pt-4">
                         <Skeleton className="h-14 w-[50%]"/>
-                        <Skeleton className="h-14 w-[50%]"/>
-                        <Skeleton className="h-14 w-[50%]"/>
+                        <Skeleton className="h-1 w-[80%]"/>
+                        <Skeleton className="h-1 w-[40%]"/>
+                        <Skeleton className="h-1 w-[60%]"/>
                     </div>
                 </div>
             </div>
